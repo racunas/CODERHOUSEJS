94d131c3afa244ceaae67811f8ed83e724cf80cc
Juan Omar Camacho <39716286+RichOmaar@users.noreply.github.com>
2021-03-30 15:04:02 -0600

    Update footer.php

--- PROYECTO-CODERHOUSE/vista/modulos/footer.php
+++ PROYECTO-CODERHOUSE/vista/modulos/footer.php
@@ -1,20 +1,20 @@
-<div class="container-fluid separador-footer pt-5 pb-5">
+<div class="container-fluid separador-footer pt-5 pb-5" style="background-color: #b1b2aa;">
     <div class="container">
         <div class="row text-center">
             <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
                 <img src="vista/assets/images/aria_icono.svg" style="max-width: 30%; line-height: 80px;" alt="">
-                <p class="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi est laudantium in voluptatibus, quo culpa quod, repellat eligendi non porro quas doloribus distinctio fugit mollitia earum reiciendis debitis, tenetur dicta?</p>
+                <p class="text-left font">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi est laudantium in voluptatibus, quo culpa quod, repellat eligendi non porro quas doloribus distinctio fugit mollitia earum reiciendis debitis, tenetur dicta?</p>
             </div>
             <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12" style="position: unset!important;">
                 <h1 style="font-style: italic; font-weight: lighter; line-height: 80px;" >Contacto</h1>
                 <div class="row">
-                    <div class="col-12">
+                    <div class="col-12 font">
                         <i class='bx bx-been-here' style="font-size: 25px; padding-right: 10px; font-weight: bold;"></i>Ciudad de México
                     </div>
-                    <div class="col-12">
+                    <div class="col-12 font">
                         <a href="tel:+5215534080960" ><i class='bx bx-phone' style="font-size: 25px; padding-right: 10px; font-weight: bold;"></i>+52 1 55 3408 0960</a>
                     </div>
-                    <div class="col-12">
+                    <div class="col-12 font">
                         <a href="mailto:hola@ariamor.com.mx" ><i class='bx bx-envelope'style="font-size: 25px; padding-right: 10px; font-weight: bold;" ></i>hola@ariamor.com.mx</a>
                     </div>
                 </div>
@@ -32,6 +32,15 @@
                         <a href="" class="iconos-redes"><i class='bx bxl-whatsapp' ></i></a>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid" style="background-color: #373d31;">
+    <div class="container">
+        <div class="row font pt-5 pb-3">
+            <div class="col-12">
+                <p style="color: #ffffff">© Copyright 2021 | ARIAMOR MX | All Rights Reserved. </p>
             </div>
         </div>
     </div>
