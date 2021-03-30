2b5fa7e63c82854fac9a6eeb3b2f3bdd7e162978
Juan Omar Camacho <39716286+RichOmaar@users.noreply.github.com>
2021-03-30 00:44:45 -0600

    Update footer.php

--- PROYECTO-CODERHOUSE/vista/modulos/footer.php
+++ PROYECTO-CODERHOUSE/vista/modulos/footer.php
@@ -7,17 +7,17 @@
             </div>
             <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12" style="position: unset!important;">
                 <h1 style="font-style: italic; font-weight: lighter; line-height: 80px;" >Contacto</h1>
-                    <div class="row">
-                        <div class="col-12">
-                            <i class='bx bx-been-here' style="font-size: 25px; padding-right: 10px; font-weight: bold;"></i>Ciudad de México
-                        </div>
-                        <div class="col-12">
-                            <a href="tel:+5215534080960" ><i class='bx bx-phone' style="font-size: 25px; padding-right: 10px; font-weight: bold;"></i>+52 1 55 3408 0960</a>
-                        </div>
-                        <div class="col-12">
-                            <a href="mailto:hola@ariamor.com.mx" ><i class='bx bx-envelope'style="font-size: 25px; padding-right: 10px; font-weight: bold;" ></i>hola@ariamor.com.mx</a>
-                        </div>
+                <div class="row">
+                    <div class="col-12">
+                        <i class='bx bx-been-here' style="font-size: 25px; padding-right: 10px; font-weight: bold;"></i>Ciudad de México
                     </div>
+                    <div class="col-12">
+                        <a href="tel:+5215534080960" ><i class='bx bx-phone' style="font-size: 25px; padding-right: 10px; font-weight: bold;"></i>+52 1 55 3408 0960</a>
+                    </div>
+                    <div class="col-12">
+                        <a href="mailto:hola@ariamor.com.mx" ><i class='bx bx-envelope'style="font-size: 25px; padding-right: 10px; font-weight: bold;" ></i>hola@ariamor.com.mx</a>
+                    </div>
+                </div>
             </div>
             <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 col-xs-12">
                 <h1 style="font-style: italic; font-weight: lighter; line-height: 80px;">Síguenos</h1>
